80a814b56858203752edaee85b8f2990b739237e
Phuc Dong <dongvuminhphuc@gmail.com>
2022-08-12 15:17:59 +0700

    Added projects

---
+++ Blackjack-Game/js/testing.js
@@ -0,0 +1,9 @@
+let castle = {
+    name: "Monselice",
+    areRoomsAvailable: true,
+    pricePerNight: 158,
+    facilities: ["wine bar", "thermal hot tub", "heated pool"]
+}
+
+console.log(castle.facilities[0]);
+console.log(castle.name);
