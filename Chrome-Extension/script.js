c24bac4f76317ee442a7975e11a5f1d7bcad5dff
Phuc Dong <dongvuminhphuc@gmail.com>
2022-08-18 10:00:31 +0700

    Updated the project

--- Chrome-Extension/script.js
+++ Chrome-Extension/script.js
@@ -2,13 +2,19 @@ let myLeads = [];
 
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
-
 let ulEl = document.querySelector("#ul-el");
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value);
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
     renderLeads();
+
+    console.log(localStorage.getItem("myLeads"));
 })
 
 function renderLeads() {
